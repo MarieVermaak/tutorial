173b1111658be0acb41070d856f39449adc21846
Marie <jeannemariebuckle@gmail.com>
2023-12-29 10:10:35 +0200

    react app

---
+++ src/testing.js
@@ -0,0 +1,11 @@
+const Testing = () => {
+  return (
+    <div>testing</div>
+  )
+}
+export default Testing
+//default exports: import testing.js 
+// named exports: --> more specific 
+// have curly braces
+// modules.exports = {testing} example --> import {Testing} from './testing'
+//module = group of things 
